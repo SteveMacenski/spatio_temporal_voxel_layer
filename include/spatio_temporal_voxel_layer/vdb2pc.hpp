cfb40cd63b33926d5289663298145c5f64c123b8
stevemacenski <stevenmacenski@gmail.com>
2018-03-06 19:43:17 -0500

    vdb2pc tool

---
+++ include/spatio_temporal_voxel_layer/vdb2pc.hpp
@@ -0,0 +1,59 @@
+/*********************************************************************
+ *
+ * Software License Agreement
+ *
+ *  Copyright (c) 2018, Simbe Robotics, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Steve Macenski (steven.macenski@simberobotics.com)
+ * Purpose: convert native vdb files to pointclouds
+ *********************************************************************/
+
+// PCL
+#include <pcl_ros/transforms.h>
+// OpenVDB
+#include <openvdb/openvdb.h>
+// STL
+#include <iostream>
+
+namespace utilities
+{
+
+class VDB2PCLPointCloud
+{
+public:
+	VDB2PCLPointCloud();
+	void SetFile(const std::string& file_name);
+	bool GetCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud);
+private:
+	std::string _file_name;
+};
+
+} // end namespace
---
+++ src/vdb2pc.cpp
@@ -0,0 +1,96 @@
+/*********************************************************************
+ *
+ * Software License Agreement
+ *
+ *  Copyright (c) 2018, Simbe Robotics, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Steve Macenski (steven.macenski@simberobotics.com)
+ *********************************************************************/
+
+#include <spatio_temporal_voxel_layer/vdb2pc.hpp>
+
+namespace utilities
+{
+
+/*****************************************************************************/
+VDB2PCLPointCloud::VDB2PCLPointCloud()
+/*****************************************************************************/
+{
+	openvdb::initialize();
+}
+
+/*****************************************************************************/
+void VDB2PCLPointCloud::SetFile(const std::string& file_name)
+/*****************************************************************************/
+{
+	_file_name = file_name;
+}
+
+/*****************************************************************************/
+bool VDB2PCLPointCloud::GetCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud)
+/*****************************************************************************/
+{
+	openvdb::io::File file(_file_name);
+	file.open();
+	openvdb::GridBase::Ptr baseGrid;
+	openvdb::DoubleGrid::Ptr grid;
+
+	bool valid_grid = false;
+
+	for (openvdb::io::File::NameIterator nameIter = file.beginName();
+                                nameIter != file.endName(); ++nameIter)
+	{
+		if (nameIter.gridName() == "SpatioTemporalVoxelLayer")
+		{
+			baseGrid = file.readGrid(nameIter.gridName());
+			grid = openvdb::gridPtrCast<openvdb::DoubleGrid>(baseGrid);
+			valid_grid = true;
+		}
+	}
+
+	if (!valid_grid)
+	{
+		std::cout << "No valid grid inside of provided file." << std::endl;
+		return false;
+	}
+
+	//populate pcl pointcloud
+  openvdb::DoubleGrid::ValueOnCIter cit_grid = grid->cbeginValueOn();
+  for (cit_grid; cit_grid; ++cit_grid)
+  {
+    const openvdb::Coord pt_index(cit_grid.getCoord()); // world or grid?
+	  cloud->push_back(pcl::PointXYZ(pt_index[0], pt_index[1], pt_index[2])); // 0,1,2 x,y,z
+	}
+
+	return true;
+}
+
+} // end namespace
