599751380adeb1c62ab5e97e4d4969c48da3d813
Marcel Soler <m.soler@ear-flap.com>
2018-12-12 11:38:54 +0100

    add a fix for not clearing local map with deactivated device

--- src/spatio_temporal_voxel_layer.cpp
+++ src/spatio_temporal_voxel_layer.cpp
@@ -389,6 +389,7 @@ bool SpatioTemporalVoxelLayer::BufferEnablerCallback(   \
       if (subcriber)
       {
         subcriber->unsubscribe();
+        this->ResetGrid(); //todo remove, make a way to clear local costmap when device is disabled
         response.message = "Disabling sensor";
       }
     }
