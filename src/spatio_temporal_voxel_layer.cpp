f16e1e75b3ad20b1c9d3e6e35b4db3281a0d3284
stevemacenski <stevenmacenski@gmail.com>
2019-12-16 13:01:21 -0800

    allowing for subscriber toggle callback

--- src/spatio_temporal_voxel_layer.cpp
+++ src/spatio_temporal_voxel_layer.cpp
@@ -283,20 +283,19 @@ void SpatioTemporalVoxelLayer::onInitialize(void)
       _observation_notifiers.push_back(filter);
     }
 
-    // TODO(stevemacenski)
-    // std::function<void(const std::shared_ptr<rmw_request_id_t>,
-    //   const std::shared_ptr<std_srvs::srv::SetBool::Request>,
-    //   std::shared_ptr<std_srvs::srv::SetBool::Response>)> toggle_srv_callback;
+    std::function<void(const std::shared_ptr<rmw_request_id_t>,
+      std_srvs::srv::SetBool::Request::SharedPtr,
+      std_srvs::srv::SetBool::Response::SharedPtr)> toggle_srv_callback;
 
-    // toggle_srv_callback = std::bind(
-    //   &SpatioTemporalVoxelLayer::BufferEnablerCallback, this,
-    //   _1, _2, _3, _observation_buffers.back(),
-    //   _observation_subscribers.back());
-    // std::string toggle_topic = source + "/toggle_enabled";
-    // auto server = rclcpp_node_->create_service<std_srvs::srv::SetBool>(
-    //   toggle_topic, toggle_srv_callback);
+    toggle_srv_callback = std::bind(
+      &SpatioTemporalVoxelLayer::BufferEnablerCallback, this,
+      _1, _2, _3, _observation_buffers.back(),
+      _observation_subscribers.back());
+    std::string toggle_topic = source + "/toggle_enabled";
+    auto server = rclcpp_node_->create_service<std_srvs::srv::SetBool>(
+      toggle_topic, toggle_srv_callback);
 
-    // _buffer_enabler_servers.push_back(server);
+    _buffer_enabler_servers.push_back(server);
 
     if (sensor_frame != "") {
       std::vector<std::string> target_frames;
@@ -670,7 +669,7 @@ void SpatioTemporalVoxelLayer::updateBounds(
   // navigation mode: clear observations, mapping mode: save maps and publish
   bool should_save = false;
   if (_map_save_duration) {
-      should_save = node_->now() - _last_map_save_time > *_map_save_duration;
+    should_save = node_->now() - _last_map_save_time > *_map_save_duration;
   }
   if (!_mapping_mode) {
     _voxel_grid->ClearFrustums(clearing_observations);
