8c2ec0e9324f370af68498d2c2eaa268eb9cc13d
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2025-03-27 22:52:39 +0000

    Added mapToWorldNoBounds function to safely retrieve world coordinates.
    Signed-off-by: Leander Stephen D'Souza <leanderdsouza1234@gmail.com>

--- spatio_temporal_voxel_layer/src/spatio_temporal_voxel_layer.cpp
+++ spatio_temporal_voxel_layer/src/spatio_temporal_voxel_layer.cpp
@@ -961,8 +961,8 @@ void SpatioTemporalVoxelLayer::clearArea(
   // convert map coords to world coords
   volume_grid::occupany_cell start_world(0, 0);
   volume_grid::occupany_cell end_world(0, 0);
-  mapToWorld(start_x, start_y, start_world.x, start_world.y);
-  mapToWorld(end_x, end_y, end_world.x, end_world.y);
+  mapToWorldNoBounds(start_x, start_y, start_world.x, start_world.y);
+  mapToWorldNoBounds(end_x, end_y, end_world.x, end_world.y);
 
   boost::recursive_mutex::scoped_lock lock(_voxel_grid_lock);
   _voxel_grid->ResetGridArea(start_world, end_world, invert_area);
