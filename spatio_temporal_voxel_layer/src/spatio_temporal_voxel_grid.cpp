7a6a4dd9bf8a18b2e317c72ec77629fe12a239f3
mini-1235 <mauricepurnawan@gmail.com>
2025-04-13 09:21:11 +0000

    Fix compilation warnings
    Signed-off-by: mini-1235 <mauricepurnawan@gmail.com>

--- spatio_temporal_voxel_layer/src/spatio_temporal_voxel_grid.cpp
+++ spatio_temporal_voxel_layer/src/spatio_temporal_voxel_grid.cpp
@@ -122,7 +122,7 @@ void SpatioTemporalVoxelGrid::ClearFrustums(
   std::vector<observation::MeasurementReading>::const_iterator it =
     clearing_readings.begin();
   for (; it != clearing_readings.end(); ++it) {
-    geometry::Frustum * frustum;
+    geometry::Frustum * frustum = nullptr;
     if (it->_model_type == DEPTH_CAMERA) {
       frustum = new geometry::DepthCameraFrustum(
         it->_vertical_fov_in_rad,
@@ -401,7 +401,7 @@ void SpatioTemporalVoxelGrid::ResetGridArea(
   boost::unique_lock<boost::mutex> lock(_grid_lock);
 
   openvdb::DoubleGrid::ValueOnCIter cit_grid = _grid->cbeginValueOn();
-  for (cit_grid; cit_grid.test(); ++cit_grid)
+  for (; cit_grid.test(); ++cit_grid)
   {
     const openvdb::Coord pt_index(cit_grid.getCoord());
     const openvdb::Vec3d pose_world = this->IndexToWorld(pt_index);
--- spatio_temporal_voxel_layer/src/spatio_temporal_voxel_layer.cpp
+++ spatio_temporal_voxel_layer/src/spatio_temporal_voxel_layer.cpp
@@ -152,7 +152,7 @@ void SpatioTemporalVoxelLayer::onInitialize(void)
   auto save_grid_callback = std::bind(
     &SpatioTemporalVoxelLayer::SaveGridCallback, this, _1, _2, _3);
   _grid_saver = node->create_service<spatio_temporal_voxel_layer::srv::SaveGrid>(
-    "save_grid", save_grid_callback, rmw_qos_profile_services_default, callback_group_);
+    "save_grid", save_grid_callback, rclcpp::SystemDefaultsQoS(), callback_group_);
 
   _voxel_grid = std::make_unique<volume_grid::SpatioTemporalVoxelGrid>(
     node->get_clock(), _voxel_size, static_cast<double>(default_value_), _decay_model,
@@ -331,7 +331,6 @@ void SpatioTemporalVoxelLayer::onInitialize(void)
       _observation_subscribers.push_back(sub);
       _observation_notifiers.push_back(filter);
     }
-
     std::function<void(const std::shared_ptr<rmw_request_id_t>,
       std_srvs::srv::SetBool::Request::SharedPtr,
       std_srvs::srv::SetBool::Response::SharedPtr)> toggle_srv_callback;
@@ -342,7 +341,7 @@ void SpatioTemporalVoxelLayer::onInitialize(void)
       _observation_subscribers.back());
     std::string toggle_topic = source + "/toggle_enabled";
     auto server = node->create_service<std_srvs::srv::SetBool>(
-      toggle_topic, toggle_srv_callback, rmw_qos_profile_services_default, callback_group_);
+      toggle_topic, toggle_srv_callback, rclcpp::SystemDefaultsQoS(), callback_group_);
 
     _buffer_enabler_servers.push_back(server);
 
